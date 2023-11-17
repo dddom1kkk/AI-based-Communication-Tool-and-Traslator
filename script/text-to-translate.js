1a95c9367c4ca2fc9581eff3641a664f1aa90aa4
Tegvaran <tegvaran328@gmail.com>
2023-11-17 15:16:58 -0800

    Added the function that checks if the input that  the user is giving is valid or not

--- script/text-to-translate.js
+++ script/text-to-translate.js
@@ -1,5 +1,5 @@
 const apiUrl = "https://api.openai.com/v1/chat/completions";
-const apiKey = "";
+const apiKey = "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";
 const SystemContent =
   "You will be provided a sentence in a language, and your task is to translate it into another language that is provided.";
 const SummarySystemContent =
@@ -13,6 +13,10 @@ let translatedTextWithDetect =
  "You will be provided a sentence in a language, and your task is to translate it into another language that is provided. If the from language is 'Detect', detect the language and output the data as an array [language, translation]" 
 
 let hasBeenSummarized = false;
+
+let previousToLanguage;
+let previousFromLanguage;
+let previousUserInput;
 
 /**
  * Sends the request for translation to openAI API and returns translated
@@ -35,71 +39,16 @@ const getTranslation = async (fromLanguage, toLanguage, userInput) => {
     ", sentence: " +
     userInput;
 
-    if (fromLanguage != "Detect Language") {
-        fetch(apiUrl, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + apiKey,
-            },
-        
-            body: JSON.stringify({
-              model: "gpt-3.5-turbo",
-              messages: [
-                {
-                  role: "system",
-                  content: SystemContent,
-                },
-                { role: "user", content: prompt },
-              ],
-            }),
-          })
-            .then((response) => response.json())
-            .then((response) => {
-              translatedText = response.choices[0].message.content;
-              showTranslatedText(translatedText);
-              document.getElementById("summaryButton").disabled = false;
-            })
-            .catch((error) => {
-              return error;
-            });
-
+    if (isValidInput(userInput, fromLanguage, toLanguage)== true) {
+      if (fromLanguage != "Detect Language") {
+        postWithoutDetect(prompt);
     } else {
-        fetch(apiUrl, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: "Bearer " + apiKey,
-            },
-        
-            body: JSON.stringify({
-              model: "gpt-3.5-turbo",
-              messages: [
-                {
-                  role: "system",
-                  content: translatedTextWithDetect,
-                },
-                { role: "user", content: prompt },
-              ],
-            }),
-          })
-            .then((response) => response.json())
-            .then((response) => {
-              translatedText = response.choices[0].message.content;
-              translatedText = translatedText.replace(/'/g, '"');
-              translatedText = JSON.parse(translatedText);
-
-              document.getElementById("fromLanguage").value = translatedText[0];
-              showTranslatedText(translatedText[1]);
-              document.getElementById("summaryButton").disabled = false;
-            })
-            .catch((error) => {
-              return error;
-            });
+        postWithoutDetect(prompt);    
+      }
+};
     }
 
-  
-};
+
 
 function getSummarization() {
   // TODO: Check if the paragraph is more than 100 words.
@@ -162,5 +111,141 @@ function translateText() {
   // Gets the translatedText from chatGPT using openAI API
   translatedText = getTranslation(fromLanguage, toLanguage, userInput);
 
+  previousFromLanguage = fromLanguage;
+  previousToLanguage = toLanguage;
+  previousUserInput = userInput;
+
   // Shows the translated text in the HTML page.
+}
+
+async function postWithoutDetect(prompt) {
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + apiKey,
+    },
+
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: SystemContent,
+        },
+        { role: "user", content: prompt },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+
+
+      translatedText = response.choices[0].message.content;
+      showTranslatedText(translatedText);
+      document.getElementById("summaryButton").disabled = false;
+    })
+    .catch((error) => {
+      return error;
+    });
+
+}
+
+async function postWithDetect(prompt) {
+  
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + apiKey,
+    },
+
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: translatedTextWithDetect,
+        },
+        { role: "user", content: prompt },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      translatedText = response.choices[0].message.content;
+      translatedText = translatedText.replace(/'/g, '"');
+      translatedText = JSON.parse(translatedText);
+
+      // Set the from language to the one that was detected.
+      document.getElementById("fromLanguage").value = translatedText[0];
+      document.getElementById("summaryButton").disabled = false;
+
+      // Show the translated text on the page.
+      showTranslatedText(translatedText[1]);
+    })
+    .catch((error) => {
+      return error;
+    });
+
+}
+
+/**
+ * This functions checks if the text that the user entered is valid.
+ * @param {String} userInput - The text the user want to translate.
+ * 
+ */
+function isValidInput(userInput, fromLanguage, toLanguage){
+
+  if (isEmpty(userInput) == true) {
+    alert("The text you want to translate can not be empty.")
+    return false;
+  } 
+  if (areLanguagesSame(fromLanguage, toLanguage) == true) {
+    alert("Please pick from language that is different from the language you want to traslate to.")
+    return false;
+  }
+  if (isUserInputTheSame(userInput, fromLanguage, toLanguage) == true) {
+    alert("Please ensure that the text you want translated is different from your last.")
+    return false;
+  }
+
+  return true;
+
+}
+
+/**
+ * This function checks if text that the user entered is empty. 
+ * @param {String} userInput - Text that the user put in.
+ * @returns {boolean} - True if the string is empty and false if not. 
+ */
+function isEmpty(userInput) {
+  if (userInput == null || userInput.trim() == "") {
+    return true;
+  }
+  return false;
+}
+
+/**
+ * Checks if the language the user wants to translate from and the language the 
+ * user wants to translate to are different. 
+ * 
+ * @param {String} fromLanguage - The language the user wants to translate from.
+ * @param {String} toLanguage - The language the user wants to translate to. 
+ */
+function areLanguagesSame(fromLanguage, toLanguage) {
+  if (fromLanguage == toLanguage){
+    return true;
+  }
+  return false;
+
+}
+
+function isUserInputTheSame(userInput, fromLanguage, toLanguage) {
+
+  if (userInput == previousUserInput && fromLanguage == previousFromLanguage && 
+    toLanguage == previousToLanguage) {
+    return true
+  }
+  return false;
 }
