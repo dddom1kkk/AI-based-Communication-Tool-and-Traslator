fe080262a4bd85c66e097a1f5942f1f1bdc308c0
Damir <dza62@sfu.ca>
2023-11-24 01:37:15 -0800

    #9. Setting up Speech API. Fixed the bug with the advanced settings button.

--- script/speak-to-translate.js
+++ script/speak-to-translate.js
@@ -1,8 +1,21 @@
+// Setting up the Speech Recognition and Synthesis API
+const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const speechToText = new SpeechToTextV1({
+  authenticator: new IamAuthenticator({
+    apikey: '{apikey}',
+  }),
+  serviceUrl: 'https://api.us-south.speech-to-text.watson.cloud.ibm.com',
+});
+
 function showSettings() {
 
   var settings = document.getElementById("edit");
 
-  if (settings.style.display == "none")
+  console.log(settings.style.display);
+  
+  if (settings.style.display == "none" || settings.style.display.length == 0)
     settings.style.display = "block";
   else
     settings.style.display = "none";
