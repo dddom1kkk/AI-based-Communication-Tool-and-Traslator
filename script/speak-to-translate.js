7aab3ed3e19332974a0ce1976212616db02ef172
Damir <dza62@sfu.ca>
2023-11-23 23:20:09 -0800

    #3. Settings button can open and close temporary section. Setup for Speech Recognition and Synthesis API page is done.

---
+++ script/speak-to-translate.js
@@ -0,0 +1,10 @@
+function showSettings() {
+
+  var settings = document.getElementById("edit");
+
+  if (settings.style.display == "none")
+    settings.style.display = "block";
+  else
+    settings.style.display = "none";
+
+}
