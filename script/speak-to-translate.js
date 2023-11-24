154fea40e0f3cf2117d629e87bdf649c194f8995
Damir <dza62@sfu.ca>
2023-11-23 17:53:39 -0800

    Issue #9. Initialized SpeechRecognitionAPI.

---
+++ script/speak-to-translate.js
@@ -0,0 +1,7 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const SpeechGrammarList =
+  window.SpeechGrammarList || window.webkitSpeechGrammarList;
+const SpeechRecognitionEvent =
+  window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
