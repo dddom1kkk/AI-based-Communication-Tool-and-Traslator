b399c588b8a9d22b665ab70e55b9562f28c06f8b
Damir <dza62@sfu.ca>
2023-11-25 18:54:10 -0800

    #9. First steps in understanding the Speech API. Tested a video file that it converts to the transcription text. Changed video resolution because of its initial size.

--- script/speak-to-translate.js
+++ script/speak-to-translate.js
@@ -1,19 +1,53 @@
-// Setting up the Speech Recognition and Synthesis API
+const fs = require('fs');
+const path = require('path');
+const ffmpeg = require('fluent-ffmpeg');
 const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
-const speechToText = new SpeechToTextV1({
-  authenticator: new IamAuthenticator({
-    apikey: '{apikey}',
-  }),
-  serviceUrl: 'https://api.us-south.speech-to-text.watson.cloud.ibm.com',
-});
+// Audio extraction using ffmpeg
+const videoFilePath = 'vid.mp4';
+const audioFilePath = './vid.wav';
+
+const command = ffmpeg(videoFilePath)
+  .audioBitrate(160)
+  .audioFrequency(44100)
+  .audioCodec('pcm_s16le')
+  .on('end', () => {
+    console.log('Audio extraction finished');
+
+    // Speech to Text using IBM Watson
+    const apiKey = 'LNI8R9zMx6-Z_yx41-9D2wuAJxk2I_IAafgIhWFbycWW';
+    const serviceUrl = 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/857622cd-52d5-49eb-b441-155b56ba1154';
+
+    const authenticator = new IamAuthenticator({ apikey: apiKey });
+    const speechToText = new SpeechToTextV1({ authenticator, serviceUrl });
+
+    const params = {
+      audio: fs.createReadStream(audioFilePath),
+      contentType: 'audio/wav',
+      model: 'en-AU_NarrowbandModel',
+    };
+
+    speechToText.recognize(params)
+      .then(response => {
+        console.log(JSON.stringify(response.result, null, 2));
+      })
+      .catch(err => {
+        console.error('Speech to Text error:', err);
+      });
+  })
+  .on('error', err => {
+    console.error('Audio extraction error:', err);
+  })
+  .save(audioFilePath);
+
+// command.run();
 
 function showSettings() {
 
   var settings = document.getElementById("edit");
 
-  console.log(settings.style.display);
+  console.log(settings.style.display)
   
   if (settings.style.display == "none" || settings.style.display.length == 0)
     settings.style.display = "block";
